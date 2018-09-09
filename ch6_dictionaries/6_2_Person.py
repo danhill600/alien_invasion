9e55d18c3688fcca913b4b0c417e3cd22c559a23
Dan Hill <dhill@witsquash.com>
2018-09-09 09:24:00 -0500

    20180909

---
+++ ch6_dictionaries/6_2_Person.py
@@ -0,0 +1,11 @@
+Ben = {
+    'first_name':'Bennett',
+    'last_name':'Foster',
+    'age':35,
+    'city':'Atlanta'
+    }
+print(Ben)
+
+
+for i in Ben:
+    print(str(Ben[i]).title())
---
+++ ch6_dictionaries/6_5_Rivers.py
@@ -0,0 +1,10 @@
+rivers = {
+    'danube':'germany',
+    'mississippi':'united states',
+    'sangamon':'united states',
+    'nile':'egypt',
+    'amazon':'brazil',
+}
+
+for i,j in rivers.items():
+    print("The " + i.title() + " river runs through " + j.title())
