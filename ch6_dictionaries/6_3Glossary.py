a3c76ae3945c6a45f56b844428f9e19f7432eb4c
Dan Hill <dhill@witsquash.com>
2018-11-05 09:33:36 -0500

    20181105

---
+++ ch6_dictionaries/6_3Glossary.py
@@ -0,0 +1,5 @@
+Glossary = {
+        'module': 'a piece of code in a separate file',
+        'class': 'a series of functions with the same namespace',
+        'inherit' : 'when one class receives all the same functions as its parent',
+        'function' : '
