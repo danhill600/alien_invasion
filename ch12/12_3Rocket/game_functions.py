b1b4f4df343f90f108d028392c38bf82ec7df4ca
Dan Hill <dhill@witsquash.com>
2019-03-18 14:09:04 -0400

    finished excercise 12_3

--- ch12/12_3Rocket/game_functions.py
+++ ch12/12_3Rocket/game_functions.py
@@ -8,6 +8,11 @@ def check_keydown_events(event, ship):
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
@@ -15,6 +20,10 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
 
 def check_events(ship):
     """Respond to keypresses and mouse events."""
