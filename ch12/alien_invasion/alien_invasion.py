526da3ae3293796d8c75c016dd1561e48b5e259d
Dan Hill <dhill@witsquash.com>
2019-04-02 04:56:46 -0400

    just added game stats

--- ch12/alien_invasion/alien_invasion.py
+++ ch12/alien_invasion/alien_invasion.py
@@ -16,19 +16,22 @@ def run_game():
     #Set the background color.
     bg_color = (137, 60, 178)
 
-    #Make a ship.
+    #Make a ship, a group of bullets and a group of aliens.
     ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+
+    #Create a fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #Start the main loop for the game.
     while True:
 
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
-
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, ship, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
