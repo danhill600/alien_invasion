18cd3e3966d42719027444e19decb26e6b534728
Dan Hill <dhill@witsquash.com>
2019-04-11 19:20:23 -0400

    used pygal to visual different dice rolls, done w ch15

--- ch15/15_3MolecularMotion.py
+++ ch15/15_3MolecularMotion.py
@@ -5,15 +5,14 @@ from random_walk import RandomWalk
 # Keep making random walks as long as the program is active.
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk(90000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
 
     # Set the size of the plotting window.
     plt.figure(figsize=(10, 6))
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolor='none', s=1)
+    plt.plot(rw.x_values, rw.y_values, linewidth=5)
 
     # Emphasize the first and last points.
     plt.scatter(0,0, c='green', edgecolors='none', s=100)
--- ch15/random_walk.py
+++ ch15/random_walk.py
@@ -18,7 +18,7 @@ class RandomWalk():
         while len(self.x_values) < self.num_points:
             # Decide which direction to go and how far to go in that direction.
             x_direction = choice([1, -1])
-            x_distance = choice ([0, 1, 2, 3, 4])
+            x_distance = choice ([0, 1, 2, 4, 4])
             x_step = x_direction * x_distance
 
             y_direction = choice([1, -1])
